3edfc1b9bcec6dc0bb849e53ec8fed9092dc27e8
ltabis <tab.rysak70@gmail.com>
2021-02-25 07:05:43 +0100

    :white_check_mark: added some unit test for the push and pop methods of the first linked list.

--- src/first.rs
+++ src/first.rs
@@ -39,3 +39,33 @@ impl List {
         }
     }
 }
+
+// Indicates that we compile this part only when testing.
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(42);
+        assert_eq!(list.pop(), Some(42));
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(1));
+    }
+}
