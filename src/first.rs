6a7a47ec3bfb30c87de14a1caf5d8d13443a716a
ltabis <tab.rysak70@gmail.com>
2021-02-25 06:57:47 +0100

    :sparkles: added the push and pop methods to the List struct.

--- src/first.rs
+++ src/first.rs
@@ -1,3 +1,4 @@
+use std::mem;
 
 // for the sake of being simple, we will skip genericity for this one
 pub struct List {
@@ -11,5 +12,30 @@ enum Link {
 
 struct Node {
     value: i32,
-    next: List
+    next: Link
+}
+
+impl List {
+    fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    fn push(&mut self, value: i32) {
+        let new_node = Box::new(Node {
+            value,
+            next: mem::replace(&mut self.head, Link::Empty)
+        });
+
+        self.head = Link::More(new_node);
+    }
+
+    fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.value)
+            }
+        }
+    }
 }
