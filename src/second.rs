12cde1757e404f647ad3f4546fc16577d0542921
ltabis <tab.rysak70@gmail.com>
2021-03-01 03:16:29 +0100

    :sparkles: added reference iterator for the second list.

--- src/second.rs
+++ src/second.rs
@@ -4,6 +4,9 @@ pub struct List<T> {
 }
 
 pub struct IntoIter<T>(List<T>);
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>
+}
 
 type Link<T> = Option<Box<Node<T>>>;
 
@@ -47,13 +50,29 @@ impl<T> List<T> {
 
     pub fn into_iter(self) -> IntoIter<T> {
         IntoIter(self)
-    } 
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.as_deref() }
+    }
 }
 
 impl<T> Iterator for IntoIter<T> {
     type Item = T;
+
     fn next(&mut self) -> Option<Self::Item> {
         self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.value
+        })
     }
 }
 
@@ -134,6 +153,22 @@ mod test {
         assert_eq!(iterator.next(), Some(3));
         assert_eq!(iterator.next(), Some(2));
         assert_eq!(iterator.next(), Some(1));
+        assert_eq!(iterator.next(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iterator = list.iter();
+
+        assert_eq!(iterator.next(), Some(&3));
+        assert_eq!(iterator.next(), Some(&2));
+        assert_eq!(iterator.next(), Some(&1));
         assert_eq!(iterator.next(), None);
     }
 }
